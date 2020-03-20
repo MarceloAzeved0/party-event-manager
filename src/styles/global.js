4c9b70f979c4f001968a49546281cd7db03c70d7
marceloAzevedo <marcelo@triider.com>
2020-03-20 16:16:51 -0300

    adjusting global

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,12 +2,12 @@ import { createGlobalStyle } from 'styled-components';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css?family=Quicksand&display=swap');
   *{
     margin: 0;
     padding: 0;
     outline: 0;
     box-sizing: border-box;
-    @import url('https://fonts.googleapis.com/css?family=Quicksand&display=swap');
 
   }
 
